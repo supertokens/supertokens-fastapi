a0c9517a52ccf7d22b5e524f7fd9e1f88907da64
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2020-08-06 18:36:40 +0530

    auth0_handler for 0.3.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extras_require = {
 
 setup(
     name="supertokens_fastapi",
-    version="1.0.0",
+    version="0.3.0",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.io",
