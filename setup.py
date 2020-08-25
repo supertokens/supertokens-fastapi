8cc71fd2d4a4837c9a3d5e49b8f081c760960908
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-08-25 23:18:55 +0530

    prepares for release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
 
 setup(
     name="supertokens_fastapi",
-    version="0.3.1",
+    version="1.0.0",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.io",
